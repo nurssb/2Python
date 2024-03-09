ad7002db7213830e4fbdf0c16c3db7aca581b9ee
nurssb <nurlanserikbaev230@gmail.com>
2024-03-09 14:09:38 +0500

    code

--- lab6/dir-and-files/ex3.py
+++ lab6/dir-and-files/ex3.py
@@ -1,6 +1,6 @@
 import os
 
-path = r'C:\Users\Erasyl\Desktop\python_1\lab\lab6\dir-and-files\text.txt'
+path = r'C:\Users\acer\Documents\sss\lab6\dir-and-files\text.txt'
 if os.access(path, os.F_OK):
      print("\n" + os.path.basename(path))
      print("\n" + os.path.dirname(path) + "\n")
--- lab6/dir-and-files/ex4.py
+++ lab6/dir-and-files/ex4.py
@@ -1,6 +1,6 @@
 import os
 
-path = r'C:\Users\Erasyl\Desktop\python_1\lab\lab6\dir-and-files\text.txt'
+path = r'C:\Users\acer\Documents\sss\lab6\dir-and-files\text.txt'
 
 with open(path, 'r') as f:
 	lines = len(f.readlines())
